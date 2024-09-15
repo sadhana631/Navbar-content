67b5f37c4c3b0d5cf07b7e322510906b487dbdd3
sadhana631 <indhusweety631@gmail.com>
2024-09-15 08:36:07 +0000

    sixth commit

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -20,21 +20,17 @@ const NotFound = () => (
       return (
         <div className={`not-found-app-container ${notFoundBgClassName}`}>
           <Navbar />
-          <div className="not-found-responsive-container">
-            <div className="not-found-container">
+          <div className="not-found-container">
               <img
                 src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
                 alt="not found"
               />
-              <h1>
+              
+              <h1>Lost Your Way</h1>
                 className={`not-found-heading ${NotFoundContextClassName}`}
-              </h1>
-              <p>
-                className={`not-found-content ${NotFoundContextClassName}`}
-              </p>
+              <p>We cannot seem to find the page</p>
             </div>
           </div>
-        </div>
       )
     }}
   </ThemeContext.Consumer>
